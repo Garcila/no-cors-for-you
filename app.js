2f9e977fe683d881f68d4ae82349ede21a7f53ab
German <germanarcila@gmail.com>
2022-01-30 11:55:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = 3000;
+const API_KEY_NEWS = process.env.API_KEY_NEWS;
+
+app.get(`/news/:query_term`, function (req, res) {
+	const url = `https://newsapi.org/v2/everything?q=${req.params.query_term}&apiKey=${API_KEY_NEWS}`;
+	axios
+		.get(url)
+		.then(response => {
+			console.log(response.data);
+			res.send(response.data.articles[0].title);
+			return response.data;
+		})
+		.catch(error => console.log(error));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
