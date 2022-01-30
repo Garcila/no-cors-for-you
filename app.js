e9aea6f0d21c177c3c503dcad9b4105107747e51
German <germanarcila@gmail.com>
2022-01-30 12:30:31 -0500

    cors all

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-// app.use(function (req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// 	next();
-// });
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 const PORT = process.env.PORT || 3000;
 const API_KEY_NEWS = process.env.API_KEY_NEWS;
