9959b3eb535c05814a2bc0fa29199d591013b273
German <germanarcila@gmail.com>
2022-01-30 11:58:34 -0500

    add process env port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const API_KEY_NEWS = process.env.API_KEY_NEWS;
 
 app.get(`/news/:query_term`, function (req, res) {
