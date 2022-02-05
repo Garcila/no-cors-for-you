cf893837b9e04625f4f9079b3a7f53d468456e2c
German <germanarcila@gmail.com>
2022-02-05 16:39:49 -0500

    add route to / to greet and explain what to use, also added favicon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const express = require("express");
 const app = express();
+const favicon = require("serve-favicon");
+
 require("dotenv").config();
 
 app.use(function (req, res, next) {
@@ -14,6 +16,7 @@ app.use(function (req, res, next) {
 
 const PORT = process.env.PORT || 3000;
 const API_KEY_NEWS = process.env.API_KEY_NEWS;
+
 //https://newsapi.org/v2/everything?q=shark&apiKey=33f90c31aec04b5e8d07242e1af4f3e3
 // app.get(`/news/:query_term`, function (req, res) {
 // 	const url = `https://newsapi.org/v2/everything?q=${req.params.query_term}&apiKey=${API_KEY_NEWS}`;
@@ -25,13 +28,22 @@ const API_KEY_NEWS = process.env.API_KEY_NEWS;
 // 		.catch(error => console.log(error));
 // });
 
-app.get("/favicon.ico", (req, res) => res.status(204));
+// 204 (no content) if it is trying to get favicon.ico
+app.get("/", (req, res) =>
+	res.send(
+		`<div style="color: rgb(255,255,255);background-color: rgb(77,77,77);height: 100vh; display: flex; flex-direction: column; justify-content: center; text-align: center">
+		<h1>👾👾👾👾👾👾👾👾👾👾👾👾👾👾👾👾👾</h1>
+		<h2>Enter the url you want to connect to</h2>
+		<h1>Exclude the https://</h1>
+		<h1>🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄</h1>
+		</div>`
+	)
+);
+
+app.use(favicon(__dirname + "/public/images/io.ico"));
+// app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.get("/:a?/:b?/:c?/:d?/:e?/:f?/:g?/:h?", function (req, res) {
-	console.log(
-		"=================================================================="
-	);
-
 	let fullURL = "";
 	const url = req.params;
 
